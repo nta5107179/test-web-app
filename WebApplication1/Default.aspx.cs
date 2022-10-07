e6a40bd24f8abfae580cbc33287d254ffa44f7b1
nta5107179 <39373156+nta5107179@users.noreply.github.com>
2022-10-07 11:02:10 +0900

    Add files via upload

---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var channel = new InMemoryChannel();
+
+            try
+            {
+                IServiceCollection services = new ServiceCollection();
+                services.Configure<TelemetryConfiguration>(config => config.TelemetryChannel = channel);
+                services.AddLogging(builder =>
+                {
+                    // Only Application Insights is registered as a logger provider
+                    builder.AddApplicationInsights(
+                        configureTelemetryConfiguration: (config) => config.ConnectionString = "InstrumentationKey=a62126d8-0c61-4cb8-a5b6-0df6d3dc09f2;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/",
+                        configureApplicationInsightsLoggerOptions: (options) => { }
+                    );
+                });
+
+                IServiceProvider serviceProvider = services.BuildServiceProvider();
+                ILogger<Default> logger = serviceProvider.GetRequiredService<ILogger<Default>>();
+
+                logger.LogInformation("Logger is working...");
+            }
+            finally
+            {
+                // Explicitly call Flush() followed by Delay, as required in console apps.
+                // This ensures that even if the application terminates, telemetry is sent to the back end.
+                channel.Flush();
+            }
+        }
+    }
+}
