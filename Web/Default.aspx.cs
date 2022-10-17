aaa06142d22e2b4858754b10e540848938eaff9d
nta5107179 <39373156+nta5107179@users.noreply.github.com>
2022-10-17 18:21:03 +0900

    Add files via upload

--- Web/Default.aspx.cs
+++ Web/Default.aspx.cs
@@ -3,6 +3,7 @@ using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -70,6 +71,7 @@ namespace Web
         {
             //GetAccessToken();
 
+            //token取得
             string result = "";
             string url = "https://login.microsoftonline.com/" + AppSettings.TenantId + "/oauth2/v2.0/token";
             HttpWebRequest hwr = WebRequest.CreateHttp(url);
@@ -93,35 +95,26 @@ namespace Web
                     result = sr.ReadToEnd();
                 }
             }
-            Response.Write(result);
-
-            //while (true)
-            //{
-            //    if (token != string.Empty)
-            //    {
-            //        Response.Write(string.Format("token:{0}", token));
-            //        break;
-            //    }
-            //    Thread.Sleep(1000);
-            //}
-
+            //Response.Write(result);
+            string token = JObject.Parse(result)["access_token"].ToString();
+            Response.Write(token);
 
             //App Service再起動
-            //string result = "";
-            //string url = "https://management.azure.com/subscriptions/eb38af0b-eef2-4638-a776-4374ff5a94a6/resourceGroups/test-resource/providers/Microsoft.Web/sites/test-web-app2022/restart?api-version=2022-03-01";
-            //HttpWebRequest hwr = WebRequest.CreateHttp(url);
-            //hwr.Method = "POST";
-            //hwr.Headers.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldCIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzYzZTllNzE3LTc5ZjgtNDQ2MS1iNTllLTE0MGQyMjQ5MjBiMy8iLCJpYXQiOjE2NjU3MTUzMzAsIm5iZiI6MTY2NTcxNTMzMCwiZXhwIjoxNjY1NzE5MzcyLCJhY3IiOiIxIiwiYWlvIjoiQVdRQW0vOFRBQUFBblBqTzJKQkliNkhPLy9kOTBQWlZQL3NpZ05Mc3F1NjdDbUp1TXc3NE1saHNlVENVSkcwRzJGNTdGS0lMWm05QU5LblU5bm5XQWljNndjTHJnNklzb082SVNUc2J2ME42ejBEMWV0cG1maDE2OURaZ0dMaEpBWWxicnBLekVHL1QiLCJhbHRzZWNpZCI6IjE6bGl2ZS5jb206MDAwNjQwMDAwNjBBQjFBMSIsImFtciI6WyJwd2QiLCJtZmEiXSwiYXBwaWQiOiIxOGZiY2ExNi0yMjI0LTQ1ZjYtODViMC1mN2JmMmIzOWIzZjMiLCJhcHBpZGFjciI6IjAiLCJlbWFpbCI6ImhpMjAyMTAxMjZAaG90bWFpbC5jb20iLCJmYW1pbHlfbmFtZSI6IlNFSSIsImdpdmVuX25hbWUiOiJLRU4iLCJncm91cHMiOlsiNGU2NTY5MTctZDY3Ni00OTE0LWJmMGUtOWQyMzc0OGFlMTM1Il0sImlkcCI6ImxpdmUuY29tIiwiaXBhZGRyIjoiMjAyLjI0Ni4yNTIuMjI2IiwibmFtZSI6IlNFSSBLRU4iLCJvaWQiOiI0ZmRmYTY1Yy05ZDM3LTRmZTktYTZjYy1lOWU5MWI2ODExNDIiLCJwdWlkIjoiMTAwMzIwMDExMDlFRDM1MyIsInJoIjoiMC5BWEFBRi1mcFlfaDVZVVMxbmhRTklra2dzMFpJZjNrQXV0ZFB1a1Bhd2ZqMk1CTndBRDguIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoiY3JzZzc4UWY2cGhpQVZod2dySmNvTlNIeTJJT1NvczhwVnJ0bjNoNEthRSIsInRpZCI6IjYzZTllNzE3LTc5ZjgtNDQ2MS1iNTllLTE0MGQyMjQ5MjBiMyIsInVuaXF1ZV9uYW1lIjoibGl2ZS5jb20jaGkyMDIxMDEyNkBob3RtYWlsLmNvbSIsInV0aSI6IkltN0xYaGN4akV5NFdwLU9ubG8wQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCIsImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfdGNkdCI6MTYxMTY1NDIzNH0.N5BZO94P1Zx8aWre6fG2ZznWc4lxFXGMT9HZfRrn3pWodW8H0gOW9GxZGfMyNSTm8RnbfQSJlUz2XddTK7bLnuykmf_FFdpFJ3Jc7s1q8Yf8fEzZjoa7PbApqSgu4r7N4KakCX7ZVXN8nikigMOs5GLZoeq5u3MXO1vQE8QBjiMucpk8hEOqsm-kZnLbCRyv2WRuRKZiLp4zSB8HTV_Qeac2ACqFgcJx8Syl1BXVhdxL3D0csmp12yatetnarSU3GaEmwQlrQZS1qJWmpI8tQQY8jNikT8hUqwQAu8fZEW7yuSpJ0A3a80TY0a9e3sfGrwG9TkR_GA8_-cYaNwF-Dg");
-            //hwr.ContentType = "application/json";
-            //hwr.ContentLength = 0;
-            //using (WebResponse wr = hwr.GetResponse())
-            //{
-            //    using (StreamReader sr = new StreamReader(wr.GetResponseStream()))
-            //    {
-            //        result = sr.ReadToEnd();
-            //    }
-            //}
-            //Response.Write(result);
+            result = "";
+            url = "https://management.azure.com/subscriptions/eb38af0b-eef2-4638-a776-4374ff5a94a6/resourceGroups/test-resource/providers/Microsoft.Web/sites/test-web-app2022/restart?api-version=2022-03-01";
+            hwr = WebRequest.CreateHttp(url);
+            hwr.Method = "POST";
+            hwr.Headers.Add("Authorization", "Bearer " + token);
+            hwr.ContentType = "application/json";
+            hwr.ContentLength = 0;
+            using (WebResponse wr = hwr.GetResponse())
+            {
+                using (StreamReader sr = new StreamReader(wr.GetResponseStream()))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+            Response.Write(result);
         }
 
         private async Task GetAccessToken()
